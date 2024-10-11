f0c776cabcfb835ecba2751eb27fa53fbe598bc4
arjunthazath <arjunta32@gmail.com>
2024-10-11 16:31:51 +0530

    ok

--- src/app/academics/page.tsx
+++ src/app/academics/page.tsx
@@ -11,49 +11,49 @@ export default function AcademicsPage() {
   return (
     <div className="flex flex-col w-full">
       {/* Section 1 */}
-      <section className="min-h-screen flex items-center justify-center px-4 sm:px-6 py-6 sm:py-8 md:py-3 lg:py-4">
+      <section className="min-h-screen flex items-center justify-center px-4 sm:px-6 py-3 md:py-5 lg:py-6">
         <div className="w-full text-center">       
           <SparklesPreview/>
         </div>
       </section>
 
       {/* Section 2 */}
-      <section className="min-h-screen flex items-center justify-center px-4 sm:px-6 py-6 sm:py-8 md:py-3 lg:py-4">
+      <section className="min-h-screen flex items-center justify-center px-4 sm:px-6 py-3 md:py-5 lg:py-6">
         <div className="w-full text-center">
           <FeaturedPrograms/>
         </div>
       </section>
 
       {/* Section 3 */}
-      <section className="min-h-screen flex items-center justify-center px-4 sm:px-6 py-6 sm:py-8 md:py-3 lg:py-4">
+      <section className="min-h-screen flex items-center justify-center px-4 sm:px-6 py-3 md:py-5 lg:py-6">
         <div className="w-full text-center">
           <Institutions/>
         </div>
       </section>
 
       {/* Section 4 */}
-      <section className="min-h-screen flex items-center justify-center px-4 sm:px-6 py-6 sm:py-8 md:py-3 lg:py-4">
+      <section className="min-h-screen flex items-center justify-center px-4 sm:px-6 py-3 md:py-5 lg:py-6">
         <div className="w-full text-center">
           <Spine/>
         </div>
       </section>
 
       {/* Section 5 */}
-      <section className="min-h-screen flex items-center justify-center px-4 sm:px-6 py-6 sm:py-8 md:py-3 lg:py-4">
+      <section className="min-h-screen flex items-center justify-center px-4 sm:px-6 py-3 md:py-5 lg:py-6">
         <div className="w-full text-center">
           <OurFacilities/>
         </div>
       </section>
 
       {/* Section 6 */}
-      <section className="min-h-screen flex items-center justify-center px-4 sm:px-6 py-6 sm:py-8 md:py-3 lg:py-4">
+      <section className="min-h-screen flex items-center justify-center px-4 sm:px-6 py-3 md:py-5 lg:py-6">
         <div className="w-full text-center">
           <OurTech/>
         </div>
       </section>
 
       {/* Section 7 */}
-      <section className="min-h-screen flex items-center justify-center px-4 sm:px-6 py-6 sm:py-8 md:py-3 lg:py-4">
+      <section className="min-h-screen flex items-center justify-center px-4 sm:px-6 py-3 md:py-5 lg:py-6">
         <div className="w-full text-center">
           <OurFaculty/>
         </div>
